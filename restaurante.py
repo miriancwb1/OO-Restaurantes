e88c82f7601c3d532a1ee8fd9496f7871bc46e42
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-05-17 11:26:08 -0300

    Add files via upload

--- restaurante.py
+++ restaurante.py
@@ -5,7 +5,7 @@ class Restaurante:
     def __init__ (self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False 
+        self._ativo = False 
         Restaurante.restaurantes.append(self)
 
 
@@ -15,9 +15,14 @@ class Restaurante:
     
     
     def listar_restaurante():
+        print(f'{'nome do restaurante'.just(25)} | {"categoria". ijust (25)} | {"status"}
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome}|{restaurante.categoria} | {restaurante.ativo}')
-
+            print(f'{restaurante.nome.ljust(24)} |{restaurante.categoria} | {restaurante.ativo}')
+    @property
+    def ativo (self):
+        return '⌧' if self._ativo else '☐' 
+    
+    
 
 restaurante_praca = Restaurante( 'prca','gourmet')
 
