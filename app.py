0846913f62fce04400fd31cdc23d68ee1f123c50
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-05-21 10:11:12 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+def main():
+    pass
+
+restaurante_praça = Restaurante ('praca' , 'gurmet')
+restaurante_mexicano = Restaurante ("mexico", "food", "mexicano")
+restaurante_japones = Restaurante ('japa', 'japones')
+restaurante_mexicano.alternar_estado()
+
+def main():
+
+    restaurante.listar_restaurante()
+
+
+if __name__== '_main_':
+    main()
+   
+
